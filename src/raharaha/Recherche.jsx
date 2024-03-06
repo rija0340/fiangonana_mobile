ba0231853994cdf29c0f28643a102239c2575a88
Raberia <rija0340@gmail.com>
2024-03-06 06:16:28 +0300

    avancemement sur datepicker dans recherhche

--- src/raharaha/Recherche.jsx
+++ src/raharaha/Recherche.jsx
@@ -1,8 +1,24 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function Recherche() {
+	const [startDate, setStartDate] = useState(new Date());
+
+	const handleDateChange = (e) => {
+		console.log(formatDate(e));
+		// setStartDate(dateValue);
+	}
+
+	const formatDate = (dateobj) => {
+		const formattedDate = `${dateobj.getFullYear()}-${String(dateobj.getMonth() + 1).padStart(2, '0')}-${String(dateobj.getDate()).padStart(2, '0')}`;
+		return formattedDate;
+	}
+
 	return (
 		<>
-			<p>ato anaty liste za zao</p>
+			<h4>Date de recherche</h4>
+			<DatePicker selected={startDate} onChange={handleDateChange} />
 		</>
 	)
 }
