312311af91788ade735a1ca059c66fc879be777b
Raberia <rija0340@gmail.com>
2024-03-08 06:16:55 +0300

    avancement sur fetchdata dans recherche

--- src/raharaha/Recherche.jsx
+++ src/raharaha/Recherche.jsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios';
+import axios from "axios";
 
 function Recherche() {
-	const [startDate, setStartDate] = useState(new Date());
+	const [date, setDate] = useState(null);
+	// const [date, setDate] = useState(new Date().toISOString().substring(0, 10));
+	const [data, setData] = useState(null);
 
 	const handleDateChange = (e) => {
-		console.log(formatDate(e));
-		// setStartDate(dateValue);
+		setDate(formatDate(e));
+		console.log(data);
+		fetchData();
+		if (data != null) {
+			const result = data.filter((item) => {
+				console.log(item.date);
+				item.date === date;
+			});
+			console.log("date");
+			console.log(date);
+			console.log(result);
+		}
+		// setData(...dataList);
 	}
 
 	const formatDate = (dateobj) => {
@@ -15,10 +30,24 @@ function Recherche() {
 		return formattedDate;
 	}
 
+	const fetchData = async (date) => {
+		try {
+			const response = await axios.get('../../data/data.json');
+			setData(response.data);
+		} catch (error) {
+			console.error('Error fetching data: ', error);
+		}
+	};
+
+	// 
+
 	return (
 		<>
 			<h4>Date de recherche</h4>
-			<DatePicker selected={startDate} onChange={handleDateChange} />
+			<DatePicker selected={date} onChange={handleDateChange} />
+			<div className="p-5">
+				<h5>Résultat de recherche</h5>
+			</div>
 		</>
 	)
 }
